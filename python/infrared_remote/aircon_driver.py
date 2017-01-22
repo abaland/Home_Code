80049df687b9a7987c3cb3ea1f3a89ec97733d78
Adrien.Baland <adrien@baland.be>
2017-01-22 23:27:13 +0900

    Added file to manually send command (custom signal) through infrared. Not working state for now.

--- python/infrared_remote/aircon_driver.py
+++ python/infrared_remote/aircon_driver.py
@@ -1,7 +1,35 @@
+"""
+This driver help converting the desired settng for the air conditioning ystem in my house to the appropriate signal
+length to send through infrared.
+
+After examination of the receiver signals (with convert_aircon_ir.py code) on many tests configuration. The following
+conclusions were taken.
+
+Off, On     : 6-th byte,  6th bit     : 0=Off,    1=On
+Temperature : 8-th byte,  1st-4th bit : 0000=16,  1000=17,  0100=18,  0010=20,  0001=24,  1111=31
+Wind speed  : 10-th byte, 1st-2nd bit : 00=auto,  10=low,  01=middle,  11=high
+Wind direct : 10-th byte, 4th-6th bit : 000=auto, 100=lowest,  010=low,  110=center,  001=high,  101=highest,  111=loop
+Sound       : 10-th byte, 7th-8th bit : 10=1beep,  01 = 2 beeps.  (THIS ONE IS NOT SURE). MAYBE 0 STOPS SOUNDS
+Mode        :
+    (1) 7-th byte, 4th-5th bit : 10=heater, 11=cooler, 01=dryer
+    (2) 9th byte, 2th-3rd bit : 00=heater, 11=cooler, 10=dryer
+Checksum    : last byte, all bits.
+    Read all but last bytes as (1,2,4,8,16,32,64,128) (convert to integer)
+    Sum these integer
+    Convert this sum to 8-bit using (1,2,4,8,16,32,64,128)
+"""
 
 #########################
 # Import global packages
 #########################
+import copy
+import signal_sender
+
+####################
+# Global parameters
+####################
+
+__author__ = 'Baland Adrien'
 
 # Signal parameters
 signal_header = [3400, 1750]  # Header of signal, before any data bit (On, Off)
@@ -16,16 +44,16 @@ repeat = [440, 17100]  # (On, Off) separating signal repetition.
 
 # Signal construction
 base_signal = [
-    '00100011',
-    '11001011',
-    '00100110',
-    '00000001',
+    '11000100',
+    '11010011',
+    '01100100',
+    '10000000',
     '00000000',
-    '00X00000',  # Controls On/Off
-    '00XXX000',  # Controls Mode (Heat/Dry/Cold/Auto)
-    '0000XXXX',  # Controls temperature
-    'XXXX0YY0',  # X : Control fan direction,  Y : Controls Mode (Heat/Dry/Cold/Auto)
-    'XXXXXXXX',  # Controls Fan Speed
+    '00000X00',  # On/Off
+    '000XX000',  # Mode (Heat/Dry/Cold)
+    'XXXX0000',  # Temperature
+    '0XX01100',  # Mode (Heat/Dry/Cold)
+    'XX0YYY10',  # X: Fan speed, Y: Fan direction
     '00000000',
     '00000000',
     '00000000',
@@ -35,6 +63,156 @@ base_signal = [
     '00000000',
     'XXXXXXXX',  # Checksum
 ]
+
+
+########################################################################################################################
+# compute_checksum
+########################################################################################################################
+# Revision History:
+#   19/01/2016 AB - Created function
+########################################################################################################################
+def compute_checksum(all_data_bytes):
+    """
+    Computes the final byte to send, which contains the checksum of all data bytes.
+
+    INPUT:
+        all_data_bytes ({'0', '1'}[8][]) all data bytes send before, to use for checksum
+
+    RETURN:
+        ({'0', '1'}[8] checksum of the sent data bytes
+    """
+
+    # Sum all data bytes as integer
+    all_bytes_sum = 0
+    for data_byte in all_data_bytes:
+
+        # Reverse the byte to fit the standard binary format. Bytes to send are written as (1, 2, 4, 8, ...)
+        reverse_byte = data_byte[::-1]
+
+        all_bytes_sum += int(reverse_byte, 2)
+
+    # Only keep the 8 lowest signicant bits to make the checksum byte.
+    all_bytes_sum %= 256
+
+    # Converts the resulting integer to binary, and reverts the string, to fit the format (1, 2, 4, 8, ...)
+    checksum = bin(all_bytes_sum)[2:].zfill(8)
+
+    ######################
+    return checksum[::-1]
+    ######################
+
+###########################
+# END convert_info_to_bits
+###########################
+
+
+########################################################################################################################
+# convert_info_to_bits
+########################################################################################################################
+# Revision History:
+#   19/01/2016 AB - Created function
+########################################################################################################################
+def convert_info_to_bits(is_turned_on, mode, temperature, wind_speed, wind_direction):
+    """
+    Converts the information to send the aircon from human-readable (turned on in heater mode at 26 degree) to the byte
+    version to be interpreted by the aircon receiver.
+
+    INPUT:
+        is_turned_on {'on', 'off'} whether aircon is on or off
+        mode {'heater', 'cold', 'dry', 'auto'} which mode the aircon is running in
+        temperature {int} temperature target
+        wind_speed {'auto', 'low', 'middle', 'high'}
+        wind_direction {'auto', 'lowest', 'low', 'middle', 'high', 'highest', 'loop'}
+
+    OUTPUT:
+        ({'0','1'}[8][]) all data bytes matching the desired settings
+    """
+
+    # Initializes the bytes to send as the base signal.
+    final_bytes = copy.deepcopy(base_signal)
+
+    # Updates values related to on/off state.
+    value = '0'
+    if is_turned_on == 'on':
+
+        value = '1'
+
+    final_bytes[5] = final_bytes[5][:5] + value + final_bytes[5][6:]
+
+    # Updates values related to aircon mode (heat, cold, dry).
+    value = ['11', '11']
+    if mode == 'heat':
+
+        value = ['10', '00']
+
+    elif mode == 'dry':
+
+        value = ['01', '10']
+
+    final_bytes[6] = final_bytes[6][:3] + value[0] + final_bytes[6][5:]
+    final_bytes[8] = final_bytes[8][:1] + value[1] + final_bytes[8][3:]
+
+    # Updates values related to aircon temperature (between 16 and 31 degrees).
+    value = '0110'
+    if 15 < temperature < 32:
+
+        value = bin(temperature - 16)[2:].zfill(4)
+
+    final_bytes[7] = value + final_bytes[7][4:]
+
+    # Updates value related to wind speed.
+    value = '00'
+    if wind_speed == 'low':
+
+        value = '10'
+
+    elif wind_speed == 'middle':
+
+        value = '01'
+
+    elif wind_speed == 'high':
+
+        value = '11'
+
+    final_bytes[9] = value + final_bytes[9][2:]
+
+    # Updates value related to wind direction.
+    value = '000'
+    if wind_direction == 'lowest':
+
+        value = '100'
+
+    elif wind_direction == 'low':
+
+        value = '010'
+
+    elif wind_direction == 'middle':
+
+        value = '110'
+
+    elif wind_direction == 'high':
+
+        value = '001'
+
+    elif wind_direction == 'highest':
+
+        value = '011'
+
+    elif wind_direction == 'loop':
+
+        value = '111'
+
+    final_bytes[9] = final_bytes[9][:3] + value + final_bytes[9][6:]
+
+    final_bytes[17] = compute_checksum(final_bytes[:17])
+
+    ###################
+    return final_bytes
+    ###################
+
+###########################
+# END convert_info_to_bits
+###########################
 
 
 ########################################################################################################################
@@ -106,50 +284,49 @@ def convert_bits_to_length(all_data_bytes):
 # Revision History:
 #   19/01/2016 AB - Created function
 ########################################################################################################################
-def convert_info_to_bits(is_turned_on, mode, temperature):
+def send_signal(is_turned_on, mode, temperature, wind_speed, wind_direction):
     """
-    Converts the information to send the aircon from human-readable (turned on in heater mode at 26 degree) to the byte
-    version to be interpreted by the aircon receiver.
+    Sends infrared signal to air conditioning system with given options.
 
     INPUT:
         is_turned_on {'on', 'off'} whether aircon is on or off
         mode {'heater', 'cold', 'dry', 'auto'} which mode the aircon is running in
         temperature {int} temperature target
+        wind_speed {'auto', 'low', 'middle', 'high'}
+        wind_direction {'auto', 'lowest', 'low', 'middle', 'high', 'highest', 'loop'}
     """
 
-    # On/Off of aircon. Initializes it to '0' (Off) and pushes it to '1' depending on argument
-    value = '0'
-    if is_turned_on == 'on':
+    data_bytes = convert_info_to_bits(is_turned_on, mode, temperature, wind_speed, wind_direction)
+    print(data_bytes)
 
-        value = '1'
+    all_lengths = convert_bits_to_length(data_bytes)
+    all_lengths = [
+        3385,    1687,     430,     420,     430,    1250,
+        430,     420,     430,    1250,     430,     420,
+        430,    1250,     430,     420,     430,    1250,
+        430,     420,     430,    1250,     430,     420,
+        430,    1250,     430,    1250,     430,     420,
+        430,    1250,     430,     420,     430,    1250,
+        430,    1250,     430,    1250,     430,    1250,
+        430,     420,     430,     420,     430,     420,
+        430,    1250,     430,     420,     430,    1250,
+        430,     420,     430,     420,     430,    1250,
+        430,     420,     430,     420,     430,     420,
+        430,     420,     430,    1250,     430,    1250,
+        430,     420,     430,    1250,     430,     420,
+        430,     420,     430,     420,     430,    1250,
+        430,     420,     430,     420,     430,     420,
+        430,    1250,     430,     420,     430,    1250,
+        430,    1250,     430]
+    print(all_lengths)
 
-    base_signal[5][2] = value
+    ir = signal_sender.SignalSendManager(21, 3800, 0.5)
 
-    # Mode of aircon. Initializes it at ['100', '00'], (Auto) and changes it to other value dependng on arguments
-    value = ['100', '00']
-    if mode == 'heat':
+    ir.send_code(all_lengths)
 
-        value = ['001', '00']
-
-    elif mode == 'dry':
-
-        value = ['010', '01']
-
-    elif mode == 'cold':
-
-        value = ['011', '11']
-
-    base_signal[6][2:5] = value[0]
-    base_signal[8][5:7] = value[1]
-
-    # Temperature of aircon. Initializes it at 22 '0110' and changes it to other value depending on argument
-    value = '0110'
-    if 15 < temperature < 32:
-
-        value = bin(temperature - 16).zfill(4)
-
-    base_signal[7][4:] = value
 
 ###########################
 # END convert_info_to_bits
 ###########################
+
+send_signal('on', 'heat', 25, 'auto', 'auto')
---
+++ python/infrared_remote/signal_sender.py
@@ -0,0 +1,369 @@
+"""
+Strongly based on https://github.com/bschwind/ir-slinger/blob/master/pyslinger.py
+
+This code handles sending messages using infrared signals with pigpio library.
+
+To understand this code, it is important to understand how signals are transmitted.
+At this level in the code, signals have been reduced to their almost-basic level, i.e. to a sequence of durations for
+"High" and "Low" signals.
+However, one lower level exists. To avoid LED consuming too much power, the IR emitter uses a specific frequency and
+duty cycle, which is only relevant for "On" state.
+When the signal to send is "High" for X milliseconds, the LED will stay Off for X milliseconds, so there is no problem.
+When the signal to send is "Low" for X milliseconds, the LED will alternate between On and Off.
+   First the X milliseconds duration is split in periods of T milliseconds = 1 / frequency.
+   For each of those T milliseconds, the LED will be On for the first T * duty_cycle ms, then Off for the rest.
+Due to this, High/Low refers to the LED state ignoring the frequency/duty_cycle, and On/Off, to the actual LED state.
+"""
+
+#########################
+# Import global packages
+#########################
+from time import sleep
+import pigpio
+
+
+# RAW IR ones and zeroes. Specify length for one and zero and simply bitbang the GPIO.
+# The default values are valid for one tested remote which didn't fit in NEC or RC-5 specifications.
+# It can also be used in case you don't want to bother with deciphering raw bytes from IR receiver:
+# i.e. instead of trying to figure out the protocol, simply define bit lengths and send them all here.
+
+####################################################################################################################
+# PulseConverter
+####################################################################################################################
+# Revision History :
+#   2017-01-22 AdBa : Class created
+####################################################################################################################
+class PulseConverter:
+    """
+    This class handles the conversion from a sequence of infrared lengths (matching the time the signal is considering
+    UP or down) to a sequence of pulse, which will match exactly when the infrared LED will be ON or OFF based on the
+    frequency and duty cycle of the signal.
+    """
+
+    ####################################################################################################################
+    # __init__
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def __init__(self, gpio_pin, frequency=36000, duty_cycle=0.33):
+        """
+        Initialization function. Assigns the signal specific information (frequency and duty_cycle), along with the GPIO
+        pin to use for sending message. Finally, initializes the array of pulses that will be sent to the pigpio library
+
+        INPUT:
+            gpio_pin (int) gpio pin where IR emitter is connected to
+            frequency (int) frequency (Hz) of the pulses to send
+            duty_cycle (double) percentage of a "HIGH" period where the IR emitter will actually be on.
+        """
+
+        # Assigns main parameters internally
+        self.gpio_pin = gpio_pin
+        self.frequency = frequency
+        self.duty_cycle = duty_cycle
+
+        # Initializes array of pulses to send
+        self.pulses = []
+
+    ###############
+    # END __init__
+    ###############
+
+    #
+    #
+    #
+
+    ####################################################################################################################
+    # add_pulse
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def add_pulse(self, gpio_on, gpio_off, length_microsec):
+        """
+        Append a pulse (On or Off state) to the pulse array.
+
+        INPUT:
+            gpio_on (int) : binary -> integer representation of the pins to turn on
+                If pin must not be turned On, 0.
+                For example, to use pin 3, gpio_on = 1000 (binary) ==> 8 (decimal).
+                To use pin 5, gpio_on = 100000 (binary) ==> 32 (decimal).
+            gpio_off (int) : binary -> integer representation of the pins to turn off
+                If pin must not be turned Off, 0.
+            delay_microsec (int) duration (micro-seconds) of the pulse.
+        """
+
+        # Creates pulse with given parameters, and append it
+        new_pulse = pigpio.pulse(gpio_on, gpio_off, length_microsec)
+        self.pulses.append(new_pulse)
+
+        #######
+        return
+        #######
+
+    ################
+    # END add_pulse
+    ################
+
+    #
+    #
+    #
+
+    ####################################################################################################################
+    # send_zero
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def send_zero(self, duration):
+        """
+        Adds "LOW" segment for a given duration to the full signal.
+
+        INPUT:
+            duration (int) duration (micro-seconds) of the LOW state.
+        """
+
+        self.add_pulse(0, 1 << self.gpio_pin, duration)
+
+        #######
+        return
+        #######
+
+    ################
+    # END send_zero
+    ################
+
+    #
+    #
+    #
+
+    ####################################################################################################################
+    # send_one
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def send_one(self, duration):
+        """
+        Adds "HIGH" segment for a given duration to the full signal.
+
+        INPUT:
+            duration (int) duration (micro-seconds) of the HIGH state.
+        """
+
+        # Computes duration of one period in micro-seconds (On + Off sequence)
+        period_time = 1000000.0 / self.frequency
+
+        # Computes duration of On/Off subparts in each of those periods
+        on_duration = int(round(period_time * self.duty_cycle))
+        off_duration = int(round(period_time * (1.0 - self.duty_cycle)))
+
+        # Computes the total number of periods required to reach the desired duration
+        total_periods = int(round(duration/period_time))
+
+        # For each period, generates two pulses, one On, one Off with the previously computed duration.
+        total_pulses = total_periods * 2
+        for i in range(total_pulses):
+
+            if i % 2 == 0:
+
+                # On subsignal.
+                self.add_pulse(1 << self.gpio_pin, 0, on_duration)
+
+            else:
+
+                # Off subsignal.
+                self.add_pulse(0, 1 << self.gpio_pin, off_duration)
+
+        #######
+        return
+        #######
+
+    ###############
+    # END send_one
+    ###############
+
+    #
+    #
+    #
+
+    ####################################################################################################################
+    # process_code
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def process_code(self, ir_all_lengths):
+        """
+        Converts a sequence of "High/Low" state durations to a sequence of "On", "Off" pulses duration.
+
+        INPUT:
+            ir_all_lengths (int[]) duration (microseconds) of the sequential High/Low states, starting with the length
+                of a "High" state.
+
+        OUTPUT:
+            (pigpio.pulse[]) pulse sequences translation of input durations.
+        """
+
+        # Initializes array of pulses to send
+        self.pulses = []
+
+        # The first signal to send is always a 1.
+        signal_type = True
+        for ir_length in ir_all_lengths:
+
+            if signal_type:
+
+                self.send_one(ir_length)
+
+            else:
+
+                self.send_zero(ir_length)
+
+            # Next signal will be the opposite of the current one (High -> Low or Low -> High)
+            signal_type = not signal_type
+
+        ###################
+        return self.pulses
+        ###################
+
+        ###################
+        # END process_code
+        ###################
+
+#####################
+# END PulseConverter
+#####################
+
+
+####################################################################################################################
+# SignalSendManager
+####################################################################################################################
+# Revision History :
+#   2017-01-22 AdBa : Class created
+####################################################################################################################
+class SignalSendManager:
+    """
+    This class handles most interactions with the pigpio module, like initialization connexion to module,
+    clearance of previous pigpio data, parametrization and start of signal to send, ...
+    """
+
+    ####################################################################################################################
+    # __init__
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def __init__(self, gpio_pin, frequency, duty_cycle):
+        """
+        Initialization function. Assigns the signal specific information (frequency and duty_cycle), along with the GPIO
+        pin to use for sending message. Initializes connexion with pigpio daemon.
+
+        INPUT:
+            gpio_pin (int) gpio pin where IR emitter is connected to
+            frequency (int) frequency (Hz) of the pulses to send
+            duty_cycle (double) percentage of a "HIGH" period where the IR emitter will actually be on.
+        """
+
+        # Initializes connexion to pigpio module.
+        self.pigpio = pigpio.pi()
+
+        # Sets pin mode to output.
+        self.pigpio.set_mode(gpio_pin, pigpio.OUTPUT)
+
+        # Initializes High/Low durations to On/Off durations converted.
+        self.pulse_converter = PulseConverter(gpio_pin, frequency, duty_cycle)
+
+    ###############
+    # END __init__
+    ###############
+
+    #
+    #
+    #
+
+    ####################################################################################################################
+    # send_code
+    ####################################################################################################################
+    # Revision History :
+    #   2017-01-22 AdBa : Function created
+    ####################################################################################################################
+    def send_code(self, ir_all_lengths):
+        """
+        Takes care of all interaction with pigpio library and sends infrared signal.
+
+        INPUT:
+            ir_all_lengths (int[]) duration (microseconds) of the sequential High/Low states, starting with the length
+                of a "High" state.
+
+        OUTPUT:
+            (int) 0 if successfull, negative number otherwise
+        """
+
+        # Makes sure all previous signal data is cleared from pigpio.
+        clear = self.pigpio.wave_clear()
+        if clear != 0:
+
+            print("Error in clearing wave!")
+            #########
+            return 1
+            #########
+
+        # Configuration the signal parameters based on the input argument
+        all_pulses = self.pulse_converter.process_code(ir_all_lengths)
+        wave_config_status = self.pigpio.wave_add_generic(all_pulses)
+        if wave_config_status < 0:
+
+            print("Error in adding wave!")
+
+            ##########################
+            return wave_config_status
+            ##########################
+
+        # Creates signal wave based on parameters sent.
+        wave_id = self.pigpio.wave_create()
+
+        # Sends wave
+        if wave_id >= 0:
+
+            print("Sending wave...")
+            wave_send_status = self.pigpio.wave_send_once(wave_id)
+
+            if wave_send_status >= 0:
+
+                print("Success! (result: %d)" % wave_send_status)
+
+            else:
+
+                print("Error! (result: %d)" % wave_send_status)
+
+                ########################
+                return wave_send_status
+                ########################
+
+        else:
+
+            print("Error creating wave: %d" % wave_id)
+
+            ###############
+            return wave_id
+            ###############
+
+        # Pauses until the signal is sent.
+        while self.pigpio.wave_tx_busy():
+
+            sleep(0.1)
+
+        # After signal has been sent, removes the wave from pigpio.
+        print("Deleting wave")
+        self.pigpio.wave_delete(wave_id)
+
+        # Closes connexion with daemon.
+        print("Terminating pigpio")
+        self.pigpio.stop()
+
+########################
+# END SignalSendManager
+########################
+
+

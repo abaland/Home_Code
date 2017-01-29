a3aa8731ddfe732dc8b17c24dc719b99dbed8d87
Adrien.Baland <adrien@baland.be>
2017-01-30 00:03:49 +0900

    Started working on android interface for aircon

--- android_remote/app/src/main/java/com/example/abaland/android_remote/MainActivity.java
+++ android_remote/app/src/main/java/com/example/abaland/android_remote/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.abaland.android_remote;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,7 +16,10 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
-public class tv_remote extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     private ConnectionFactory factory = new ConnectionFactory();
     private Connection connection;
@@ -35,18 +36,10 @@ public class tv_remote extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tv_remote);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ////////////////
         // Start added.
@@ -86,7 +79,7 @@ public class tv_remote extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tv_remote, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
